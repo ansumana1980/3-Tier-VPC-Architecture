a333dbcd30b7127b7b66b94b896fe9a5cea3fdfe
ansumana1980 <ansurogersbusiness@gmail.com>
2026-04-16 21:20:34 -0400

    Resolve merge conflict in 3-tier VPC variables

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
-=======
 # Environment variables
 variable "region" {
   description = "AWS region"
   type        = string
 }
 
->>>>>>> f4440188ab6c253222ef11af85a5fdec83784071
 variable "project_name" {
   description = "Project name used for naming resources"
   type        = string
@@ -22,8 +19,6 @@ variable "common_tags" {
   type        = map(string)
 }
 
-<<<<<<< HEAD
-=======
 variable "availability_zone_1" {
   description = "Availability Zone 1"
   type        = string
@@ -33,7 +28,6 @@ variable "availability_zone_2" {
   description = "Availability Zone 2"
   type        = string
 }
->>>>>>> f4440188ab6c253222ef11af85a5fdec83784071
 
 # VPC variables
 variable "vpc_cidr_block" {
@@ -42,63 +36,31 @@ variable "vpc_cidr_block" {
 }
 
 variable "enable_dns_support" {
-<<<<<<< HEAD
   description = "Enable DNS support in the VPC"
-=======
-  description = "Enable DNS support"
->>>>>>> f4440188ab6c253222ef11af85a5fdec83784071
   type        = bool
 }
 
 variable "enable_dns_hostnames" {
-<<<<<<< HEAD
   description = "Enable DNS hostnames in the VPC"
   type        = bool
 }
 
-variable "availability_zone_1" {
-  description = "Availability Zone 1"
-  type        = string
-}
-
-variable "availability_zone_2" {
-  description = "Availability Zone 2"
-  type        = string
-}
-
 variable "map_public_ip_on_launch" {
   description = "Assign public IP addresses to resources launched in public subnets"
-=======
-  description = "Enable DNS hostnames"
-  type        = bool
-}
-
-variable "map_public_ip_on_launch" {
-  description = "Assign public IPs to public subnets"
->>>>>>> f4440188ab6c253222ef11af85a5fdec83784071
   type        = bool
 }
 
 variable "private_map_public_ip_on_launch" {
-<<<<<<< HEAD
   description = "Assign public IP addresses to resources launched in private subnets"
-=======
-  description = "Assign public IPs to private subnets"
->>>>>>> f4440188ab6c253222ef11af85a5fdec83784071
   type        = bool
 }
 
 variable "public_subnet_az1_cidr" {
-<<<<<<< HEAD
   description = "CIDR block for public subnet in Availability Zone 1"
-=======
-  description = "Public subnet AZ1 CIDR"
->>>>>>> f4440188ab6c253222ef11af85a5fdec83784071
   type        = string
 }
 
 variable "public_subnet_az2_cidr" {
-<<<<<<< HEAD
   description = "CIDR block for public subnet in Availability Zone 2"
   type        = string
 }
@@ -120,18 +82,5 @@ variable "private_data_subnet_az1_cidr" {
 
 variable "private_data_subnet_az2_cidr" {
   description = "CIDR block for private data subnet in Availability Zone 2"
-=======
-  description = "Public subnet AZ2 CIDR"
-  type        = string
-}
-
-variable "private_subnet_az1_cidr" {
-  description = "Private subnet AZ1 CIDR"
-  type        = string
-}
-
-variable "private_subnet_az2_cidr" {
-  description = "Private subnet AZ2 CIDR"
->>>>>>> f4440188ab6c253222ef11af85a5fdec83784071
   type        = string
 }
